fe755828cdfe15b49b64dced55037f1de9f8c605
Christopher Prener <chris.prener@slu.edu>
2019-04-18 11:40:39 -0500

    add csb data

--- source/buildData.R
+++ source/buildData.R
@@ -1,6 +1,8 @@
 # create local data sources for tutorial
 library(here)
 library(gateway)
+library(readr)
+library(stlcsb)
 library(usethis)
 
 # download geocoders and save
@@ -9,3 +11,7 @@ use_data(geocoder)
 
 geocoder_s <- gw_build_geocoder(style = "short", class = "tibble", return = "coords")
 use_data(geocoder_s)
+
+# download stlcsb data and save
+csb <- csb_get_data(years = 2017:2018)
+write_csv(csb, path = here("data", "csb.csv"))
